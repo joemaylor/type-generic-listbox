9f0330f1d7348f3c407bf9c66136a927bc3e5036
joemaylor <77979674+joemaylor@users.noreply.github.com>
2023-05-05 08:52:53 +0100

    init commit

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+import VSelect from '@/components/VSelect.vue'
+import { ref } from 'vue'
+
+interface Person {
+  id: number
+  name: string
+}
+
+interface Car {
+  id: number
+  make: string
+  model: string
+}
+
+const people: Person[] = [
+  {
+    id: 1,
+    name: 'Bilbo'
+  },
+  {
+    id: 2,
+    name: 'Gandalf'
+  },
+  {
+    id: 3,
+    name: 'Frodo'
+  }
+]
+
+const cars: Car[] = [
+  {
+    id: 1,
+    make: 'Ford',
+    model: 'Fiesta',
+  },
+  {
+    id: 2,
+    make: 'Citroën',
+    model: 'Saxo'
+  },
+  {
+    id: 3,
+    make: 'Nissan',
+    model: 'Leaf'
+  },
+]
+
+const person = ref<number>()
+const car = ref<number>()
+
+const invalidPerson = ref<number>()
+const invalidCar = ref<number>()
+</script>
+
+<template>
+  <h2 class="text-lg font-bold">Valid</h2>
+  <span>Person Value: {{ person }}</span>
+  <VSelect label="Person" v-model="person" :items="people" :value-key="p => p.id" label-key="name" />
+  <span>Car Value: {{ car }}</span>
+  <VSelect label="Car" v-model="car" :items="cars" :value-key="c => c.id" :label-key="c => `${c.make} ${c.model}`" />
+
+  <h2 class="text-lg font-bold mt-6">Type Errors</h2>
+  <span>Person Value: {{ invalidPerson }}</span>
+  <VSelect label="Person" v-model="invalidPerson" :items="people" :value-key="p => p.invalidKey" label-key="invalidKey" />
+  <span>Car Value: {{ invalidCar }}</span>
+  <VSelect label="Car" v-model="invalidCar" :items="cars" :value-key="c => c.car_id" :label-key="c => `${c.year} ${c.model}`" />
+</template>
---
+++ src/components/VSelect.vue
@@ -0,0 +1,57 @@
+<script 
+  setup 
+  lang="ts" 
+  generic="TValue extends string | number | boolean  | null | undefined, TList = {[key: string]: any}"
+>
+
+import {
+  Listbox,
+  ListboxLabel,
+  ListboxButton,
+  ListboxOptions,
+  ListboxOption,
+} from '@headlessui/vue'
+import { computed } from 'vue';
+
+const props = defineProps<{
+  modelValue: TValue
+  label: string
+  items: TList[]
+  valueKey: keyof TList | ((o: TList) => Exclude<TValue, undefined>)
+  labelKey: keyof TList | ((o: TList) => string)
+}>()
+
+defineEmits<{
+  (e: 'update:modelValue', value: TValue): void
+}>()
+
+const valueFn = computed(() => typeof props.valueKey === 'function' ? props.valueKey : ((o: TList) => o[props.valueKey as keyof TList] as Exclude<TValue, undefined>))
+const displayFn = computed(() => typeof props.labelKey === 'function' ? props.labelKey : ((o: TList) => o[props.labelKey as keyof TList]))
+
+const selectedItem = computed(() => props.items.find(valueFn.value))
+</script>
+
+<template>
+  <div class="w-72">
+    <Listbox @update:model-value="$emit('update:model-value', $event)">
+      <ListboxLabel>{{ label }}</ListboxLabel>
+      <div class="relative">
+        <ListboxButton class="relative w-full border text-left">
+          <span class="block truncate">{{ selectedItem ? displayFn(selectedItem) : 'Please Select...' }}</span>
+        </ListboxButton>
+        <ListboxOptions class="absolute max-h-60 z-10 bg-white w-full overflow-auto py-1 border">
+          <ListboxOption v-slot="{ active, selected }" v-for="item in items" :key="String(valueFn(item))"
+            :value="valueFn(item)" as="template"
+          >
+            <li class="relative select-none" :class="{
+              'bg-blue-100': active,
+              'bg-blue-50': selected && !active
+            }">
+              <span>{{ displayFn(item) }}</span>
+            </li>
+          </ListboxOption>
+        </ListboxOptions>
+      </div>
+    </Listbox>
+  </div>
+</template>
